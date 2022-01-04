584fb89cb1be303571c2625aab35a78cf5237a4a
muresanu marius <mariusmuresanu@yahoo.com>
2022-01-04 04:07:56 +0200

    Add db context to validator

--- ModelValidators/FruitValidator.cs
+++ ModelValidators/FruitValidator.cs
@@ -9,10 +9,13 @@ namespace FruitsApp.ModelValidators
 {
     public class FruitValidator : AbstractValidator<Fruit>
 	{
-		public FruitValidator()
+		
+		public FruitValidator(FruitsDbContext context)
 		{
-			RuleFor(x => x.MarketPrice).InclusiveBetween(5, 1000);
-			RuleFor(x => x.DateAdded).LessThan(DateTime.Now);
+			RuleFor(x => x.MarketPrice)
+				.InclusiveBetween(5, context.Fruits.Select(f => f.MarketPrice).Max());
+			RuleFor(x => x.DateAdded)
+				.LessThan(DateTime.Now);
 			RuleFor(x => x.FruitUpkeepDifficulty)
 				.Equal(FruitUpkeepDifficulty.Easy)
 				.When(x => x.MarketPrice >= 5 && x.MarketPrice <= 10);
